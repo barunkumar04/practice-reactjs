6d308986c80f3834d5e0175862e58e5d68d9a0fa
Barun Kumar <barun.kumar04@gmail.com>
2021-07-13 05:33:14 +0530

    First component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(){
+    return(
+        <header>
+            <h1>React</h1>
+        </header>
+    );
+}
+
+function Body(){
+    return(
+        <body>
+            <h2>An open source framework</h2>
+        </body>
+    );
+}
+
+function Footer(){
+    return(
+        <footer>
+            <h3>Creates beautiful UI</h3>
+        </footer>
+    );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+     <Body/>
+     <Footer/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 
-  React.createElement("ul", null,
-    React.createElement("li", null, "Monday"),
-    React.createElement("li", null, "Friday"),
-    React.createElement("li", null, "Sunday")),
+  <App/>,
   document.getElementById('root')
 );
 
