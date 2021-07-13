4d49d30b948f69abf1c6a3e923f91cbef2633dde
Barun Kumar <barun.kumar04@gmail.com>
2021-07-13 06:19:56 +0530

    Dynamic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
 
-function Header(){
+function Header(props){
+    console.log(props)
     return(
         <header>
-            <h1>React</h1>
+            <h1>Barun is learning React</h1>
+            <h1>{props.who} learning React</h1>
         </header>
     );
 }
 
-function Body(){
+function Body(props){
     return(
         <body>
-            <h2>An open source framework</h2>
+            <h2>
+                <p>This is an open source framework</p>
+                <p>This is {props.what}</p>
+            </h2>
         </body>
     );
 }
 
-function Footer(){
+function Footer(props){
     return(
         <footer>
-            <h3>Creates beautiful UI</h3>
+            <h3>
+                <p>Creates beautiful UI.</p>
+                <p>Reactjs is creating beautiful UI since {props.since}</p>
+            </h3>
         </footer>
     );
 }
@@ -27,9 +35,9 @@ function Footer(){
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <Body/>
-     <Footer/>
+     <Header who="We are"/>
+     <Body what="a JavaScript library"/>
+     <Footer since= {new Date().getFullYear()}/>
     </div>
   );
 }
