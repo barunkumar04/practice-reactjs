a24c5cbae53d634bb38e52d2d7dfb5e8982d4d38
Barun Kumar <barun.kumar04@gmail.com>
2021-07-15 22:07:31 +0530

    React.Fragment

---
+++ src/App2.js
@@ -0,0 +1,16 @@
+
+function Feedback(){
+    return (
+    <h4>Please share feedback.</h4>
+    );
+}
+
+function App2() {
+  return (
+    <div className="App2">
+     <Feedback/>
+    </div>
+  );
+}
+
+export default App2;
